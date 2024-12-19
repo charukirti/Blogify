99215dad2ebb3c9a7654506ff013a1fdf4090967
charukirti <charukirticc112002@gmail.com>
2024-12-19 23:06:29 +0530

    configure environment variables

---
+++ src/conf/conf.js
@@ -0,0 +1,15 @@
+const conf = {
+    appEndpoint: import.meta.env.VITE_APP_ENDPOINT,
+    appProjectID: import.meta.env.VITE_APP_PROJECT_ID,
+    appDatabaseID: import.meta.env.VITE_APP_DATABASE_ID,
+    userCollectionID: import.meta.env.VITE_APP_USER_COLLECTION_ID,
+    blogsCollectionID: import.meta.env.VITE_APP_BLOGS_COLLECTION_ID,
+    analyticsCollectionID: import.meta.env.VITE_APP_ANALYTICS_COLLECTION_ID,
+    commentsCollectionID: import.meta.env.VITE_APP_COMMENTS_COLLECTION_ID,
+    likesCollectionID: import.meta.env.VITE_APP_LIKES_COLLECTION_ID,
+    thumbnailStoreID: import.meta.env.VITE_APP_THUMBNAIL_STORE_ID,
+    avatarStoreID: import.meta.env.VITE_APP_AVATARS_STORE_ID,
+
+};
+
+export default conf;
