6d29f57feade11edadf4c559cf0795dfbcf20bdb
charukirti <charukirticc112002@gmail.com>
2025-01-02 11:50:25 +0530

    fix: update Loader component border color for visiblity

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,7 +1,7 @@
 export default function Loader (){
     return (
         <div className="flex justify-center items-center h-screen">
-            <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-slate-800"></div>
+            <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-slate-300"></div>
         </div>
     )
 }
