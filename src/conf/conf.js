0147c779d6fa9763fa6810091b32d17fba622aac
charukirti <charukirticc112002@gmail.com>
2025-01-06 14:38:07 +0530

    feat: add viewsCollectionID to configuration for tracking page views

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -7,6 +7,7 @@ const conf = {
     analyticsCollectionID: import.meta.env.VITE_APP_ANALYTICS_COLLECTION_ID,
     commentsCollectionID: import.meta.env.VITE_APP_COMMENTS_COLLECTION_ID,
     likesCollectionID: import.meta.env.VITE_APP_LIKES_COLLECTION_ID,
+    viewsCollectionID: import.meta.env.VITE_APP_VIEWS_COLLECTION_ID,
     thumbnailStoreID: import.meta.env.VITE_APP_THUMBNAIL_STORE_ID,
     avatarStoreID: import.meta.env.VITE_APP_AVATARS_STORE_ID,
     tinyMceApiKey: import.meta.env.VITE_APP_VITE_APP_TINY_MCE
