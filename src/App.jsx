4d8de6fd8acc992888dc80f4544f9a61251ca464
charukirti <charukirticc112002@gmail.com>
2025-01-07 16:03:10 +0530

    feat: implement code-splitting for better performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
+import { lazy, Suspense, useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
-
-import Blogs from "./pages/Blogs";
-import Dashboard from "./pages/Dashboard";
-import Blog from "./pages/Blog";
-import SignIn from "./ui/auth/SignIn";
-import SignUp from "./ui/auth/SignUp";
-import Analytics from "./ui/analytics/Analytics";
-import AppLayout from "./ui/AppLayout";
-import { useEffect, useState } from "react";
+import { login, logout } from "./store/authSlice";
 import { useDispatch } from "react-redux";
 import authservice from "./services/auth";
-import { login, logout } from "./store/authSlice";
-import CreateBlog from "./ui/blogs/CreateBlog";
-import ProtectedRoute from "./ui/ProtectedRoute";
 import Loader from "./ui/Loader";
-import Profile from "./pages/Profile";
-import PageNotFound from "./ui/PageNotFound";
-import EditPost from "./pages/EditPost";
+import AppLayout from './ui/AppLayout'
+const Blogs = lazy(() => import("./pages/Blogs"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Blog = lazy(() => import("./pages/Blog"));
+const SignIn = lazy(() => import("./ui/auth/SignIn"));
+const SignUp = lazy(() => import("./ui/auth/SignUp"));
+const Analytics = lazy(() => import("./ui/analytics/Analytics"));
+const CreateBlog = lazy(() => import("./ui/blogs/CreateBlog"));
+const ProtectedRoute = lazy(() => import("./ui/ProtectedRoute"));
+const Profile = lazy(() => import("./pages/Profile"));
+const PageNotFound = lazy(() => import("./ui/PageNotFound"));
+const EditPost = lazy(() => import("./pages/EditPost"));
 
 function App() {
   const [loading, setIsLoading] = useState(true);
@@ -50,92 +49,94 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route
-            path="/signin"
-            element={
-              <ProtectedRoute isAuthenticated={false}>
-                <SignIn />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/signup"
-            element={
-              <ProtectedRoute isAuthenticated={false}>
-                <SignUp />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute isAuthenticated={true}>
-                <Blogs />
-              </ProtectedRoute>
-            }
-          />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route
+              path="/signin"
+              element={
+                <ProtectedRoute isAuthenticated={false}>
+                  <SignIn />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <ProtectedRoute isAuthenticated={false}>
+                  <SignUp />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute isAuthenticated={true}>
+                  <Blogs />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path="/blog/:id"
-            element={
-              <ProtectedRoute isAuthenticated>
-                <Blog />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/create"
-            element={
-              <ProtectedRoute isAuthenticated>
-                <CreateBlog />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/edit/:id"
-            element={
-              <ProtectedRoute isAuthenticated>
-                <EditPost />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute isAuthenticated>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path="/blog/:id"
+              element={
+                <ProtectedRoute isAuthenticated>
+                  <Blog />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/create"
+              element={
+                <ProtectedRoute isAuthenticated>
+                  <CreateBlog />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/edit/:id"
+              element={
+                <ProtectedRoute isAuthenticated>
+                  <EditPost />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute isAuthenticated>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path="/dashboard/analytics"
-            element={
-              <ProtectedRoute isAuthenticated>
-                <Analytics />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute isAuthenticated>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="*"
-            element={
-              <ProtectedRoute isAuthenticated>
-                <PageNotFound />
-              </ProtectedRoute>
-            }
-          />
-        </Route>
-      </Routes>
+            <Route
+              path="/dashboard/analytics"
+              element={
+                <ProtectedRoute isAuthenticated>
+                  <Analytics />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute isAuthenticated>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <ProtectedRoute isAuthenticated>
+                  <PageNotFound />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
