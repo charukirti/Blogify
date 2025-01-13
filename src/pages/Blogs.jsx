4f5a097351f889b50805d0a0921c461905e000c5
charukirti <charukirticc112002@gmail.com>
2025-01-13 20:38:16 +0530

    refactor: lazy load BlogCard and TagsSection components for improved performance

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,10 +1,16 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  lazy,
+  Suspense,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllPosts } from "../store/fetchPostSlice";
-import BlogCard from "../ui/home/BlogCard";
-import TagsSection from "../ui/home/TagsSection";
+const BlogCard = lazy(() => import("../ui/home/BlogCard"));
+const TagsSection = lazy(() => import("../ui/home/TagsSection"));
 import Loader from "../ui/Loader";
-
 
 export default function Blogs() {
   const dispatch = useDispatch();
@@ -15,13 +21,18 @@ export default function Blogs() {
     dispatch(fetchAllPosts());
   }, [dispatch]);
 
-  const handleTagClick =useCallback( (tag) => {
-    setSelectedTag(tag === selectedTag ? null : tag);
-  }, [selectedTag]);
+  const handleTagClick = useCallback(
+    (tag) => {
+      setSelectedTag(tag === selectedTag ? null : tag);
+    },
+    [selectedTag]
+  );
 
-  const filteredPosts = useMemo(() =>selectedTag
-    ? posts?.filter((post) => post.tags?.includes(selectedTag))
-    : posts,
+  const filteredPosts = useMemo(
+    () =>
+      selectedTag
+        ? posts?.filter((post) => post.tags?.includes(selectedTag))
+        : posts,
     [selectedTag, posts]
   );
 
@@ -30,15 +41,28 @@ export default function Blogs() {
   }
 
   return (
-    <main className="container mx-auto px-4 py-8">
-      {posts && <TagsSection posts={posts} onTagClick={handleTagClick} />}
-      <h1 className="text-3xl font-bold mb-8 text-gray-200">
-        Blog Posts {selectedTag && `- Tagged with ${selectedTag}`}
-      </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {filteredPosts?.map((post) => (
-          <BlogCard key={post.$id} post={post} />
-        ))}
+    <main className="max-w-7xl mx-auto px-4">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="lg:col-span-2 mt-3">
+          <h1 className="text-3xl font-bold mb-8 w-fit text-gray-200 bg-black/20 backdrop-blur-sm sticky top-24 z-30">
+            Blog Posts {selectedTag && `- Tagged with ${selectedTag}`}
+          </h1>
+          <div className="lg:space-y-8">
+            {filteredPosts?.map((post) => (
+              <Suspense fallback={<Loader />} key={post.$id}>
+                <BlogCard key={post.$id} post={post} />
+              </Suspense>
+            ))}
+          </div>
+        </div>
+
+        <div className="lg:sticky lg:top-24 lg:self-start h-fit mt-3 ">
+          {posts && (
+            <Suspense fallback={<Loader />}>
+              <TagsSection posts={posts} onTagClick={handleTagClick} />
+            </Suspense>
+          )}
+        </div>
       </div>
     </main>
   );
